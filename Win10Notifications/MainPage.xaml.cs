14e4785aa2665130af55e6f66fe4eed48b5503a7
ignuxs34 <ignuxs34@gmail.com>
2017-01-29 00:13:56 +0200

    Project files, added notification listener, added rfcomm server

---
+++ Win10Notifications/MainPage.xaml.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Win10Notifications
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private UserNotificationListener listener;
+
+        private StreamSocket socket;
+        private DataWriter writer;
+        private RfcommServiceProvider rfcommProvider;
+        private StreamSocketListener socketListener;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Initialize();
+        }
+
+        private void ListenButton_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeRfcommServer();
+        }
+
+        private void ClearNotificationsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear all notifications. Use with caution.
+            listener.ClearNotifications();
+        }
+
+        private async void Initialize()
+        {
+            // Get the listener
+            listener = UserNotificationListener.Current;
+
+            // And request access to the user's notifications (must be called from UI thread)
+            UserNotificationListenerAccessStatus accessStatus = await listener.RequestAccessAsync();
+
+            switch (accessStatus)
+            {
+                // This means the user has granted access.
+                case UserNotificationListenerAccessStatus.Allowed:
+
+                    // Yay! Proceed as normal
+                    break;
+
+                // This means the user has denied access.
+                // Any further calls to RequestAccessAsync will instantly
+                // return Denied. The user must go to the Windows settings
+                // and manually allow access.
+                case UserNotificationListenerAccessStatus.Denied:
+
+                    // Show UI explaining that listener features will not
+                    // work until user allows access.
+                    var dialog = new MessageDialog("You need to turn on access to notifications in privacy settings!", "Error");
+                    dialog.Commands.Add(new UICommand { Label = "Close", Id = 0 });
+                    var res = await dialog.ShowAsync();
+                    if ((int)res.Id == 0)
+                    {
+                        Application.Current.Exit();
+                    }
+                    break;
+
+                // This means the user closed the prompt without
+                // selecting either allow or deny. Further calls to
+                // RequestAccessAsync will show the dialog again.
+                case UserNotificationListenerAccessStatus.Unspecified:
+
+                    // Show UI that allows the user to bring up the prompt again
+                    break;
+            }
+
+            // TODO: Request/check Listener access via UserNotificationListener.Current.RequestAccessAsync
+
+            BackgroundAccessStatus backgroundStatus = await BackgroundExecutionManager.RequestAccessAsync();
+
+            switch (backgroundStatus)
+            {
+                case BackgroundAccessStatus.DeniedByUser:
+                    var dialog = new MessageDialog("You need to turn on access to background apps in privacy settings!", "Error");
+                    var res = await dialog.ShowAsync();
+                    break;
+            }
+
+            // TODO: Request/check background task access via BackgroundExecutionManager.RequestAccessAsync
+
+            // If background task isn't registered yet
+            if (!BackgroundTaskRegistration.AllTasks.Any(i => i.Value.Name.Equals("UserNotificationChanged")))
+            {
+                // Specify the background task
+                var builder = new BackgroundTaskBuilder()
+                {
+                    Name = "UserNotificationChanged"
+                };
+
+                // Set the trigger for Listener, listening to Toast Notifications
+                builder.SetTrigger(new UserNotificationChangedTrigger(NotificationKinds.Toast));
+
+                // Register the task
+                builder.Register();
+            }
+
+            UpdateNotifications();
+        }
+
+        public async void UpdateNotifications()
+        {
+            // Get the toast notifications
+            IReadOnlyList<UserNotification> notifs = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+
+            if (notifs.Count != 0)
+            {
+                // Select the first notification
+                UserNotification notif = notifs[0];
+
+                // Get the app's display name
+                string appDisplayName = notif.AppInfo.DisplayInfo.DisplayName;
+
+                // Get the app's logo
+                BitmapImage appLogo = new BitmapImage();
+                RandomAccessStreamReference appLogoStream = notif.AppInfo.DisplayInfo.GetLogo(new Size(16, 16));
+                await appLogo.SetSourceAsync(await appLogoStream.OpenReadAsync());
+
+                // Get the toast binding, if present
+                NotificationBinding toastBinding = notif.Notification.Visual.GetBinding(KnownNotificationBindings.ToastGeneric);
+
+                if (toastBinding != null)
+                {
+                    // And then get the text elements from the toast binding
+                    IReadOnlyList<AdaptiveNotificationText> textElements = toastBinding.GetTextElements();
+
+                    // Treat the first text element as the title text
+                    string titleText = textElements.FirstOrDefault()?.Text;
+
+                    // We'll treat all subsequent text elements as body text,
+                    // joining them together via newlines.
+                    string bodyText = string.Join("\n", textElements.Skip(1).Select(t => t.Text));
+
+                    MessageTextBox.Text = bodyText;
+
+                    SendMessage();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes the server using RfcommServiceProvider to advertise the Chat Service UUID and start listening
+        /// for incoming connections.
+        /// </summary>
+        private async void InitializeRfcommServer()
+        {
+            ListenButton.IsEnabled = false;
+            DisconnectButton.IsEnabled = true;
+
+            try
+            {
+                rfcommProvider = await RfcommServiceProvider.CreateAsync(RfcommServiceId.FromUuid(Constants.RfcommChatServiceUuid));
+            }
+            // Catch exception HRESULT_FROM_WIN32(ERROR_DEVICE_NOT_AVAILABLE).
+            catch (Exception ex) when ((uint)ex.HResult == 0x800710DF)
+            {
+                // The Bluetooth radio may be off.
+                NotifyUser("Make sure your Bluetooth Radio is on: " + ex.Message, NotifyType.ErrorMessage);
+                ListenButton.IsEnabled = true;
+                DisconnectButton.IsEnabled = false;
+                return;
+            }
+
+
+            // Create a listener for this service and start listening
+            socketListener = new StreamSocketListener();
+            socketListener.ConnectionReceived += OnConnectionReceived;
+            var rfcomm = rfcommProvider.ServiceId.AsString();
+
+            await socketListener.BindServiceNameAsync(rfcommProvider.ServiceId.AsString(),
+                SocketProtectionLevel.BluetoothEncryptionAllowNullAuthentication);
+
+            // Set the SDP attributes and start Bluetooth advertising
+            InitializeServiceSdpAttributes(rfcommProvider);
+
+            try
+            {
+                rfcommProvider.StartAdvertising(socketListener, true);
+            }
+            catch (Exception e)
+            {
+                // If you aren't able to get a reference to an RfcommServiceProvider, tell the user why.  Usually throws an exception if user changed their privacy settings to prevent Sync w/ Devices.  
+                NotifyUser(e.Message, NotifyType.ErrorMessage);
+                ListenButton.IsEnabled = true;
+                DisconnectButton.IsEnabled = false;
+                return;
+            }
+
+            NotifyUser("Listening for incoming connections", NotifyType.StatusMessage);
+        }
+
+        /// <summary>
+        /// Used to display messages to the user
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="type"></param>
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+                    break;
+            }
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusPanel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        public enum NotifyType
+        {
+            StatusMessage,
+            ErrorMessage
+        };
+
+        /// <summary>
+        /// Creates the SDP record that will be revealed to the Client device when pairing occurs.  
+        /// </summary>
+        /// <param name="rfcommProvider">The RfcommServiceProvider that is being used to initialize the server</param>
+        private void InitializeServiceSdpAttributes(RfcommServiceProvider rfcommProvider)
+        {
+            var sdpWriter = new DataWriter();
+
+            // Write the Service Name Attribute.
+            sdpWriter.WriteByte(Constants.SdpServiceNameAttributeType);
+
+            // The length of the UTF-8 encoded Service Name SDP Attribute.
+            sdpWriter.WriteByte((byte)Constants.SdpServiceName.Length);
+
+            // The UTF-8 encoded Service Name value.
+            sdpWriter.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+            sdpWriter.WriteString(Constants.SdpServiceName);
+
+            // Set the SDP Attribute on the RFCOMM Service Provider.
+            rfcommProvider.SdpRawAttributes.Add(Constants.SdpServiceNameAttributeId, sdpWriter.DetachBuffer());
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        public void KeyboardKey_Pressed(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        private async void SendMessage()
+        {
+            // There's no need to send a zero length message
+            if (MessageTextBox.Text.Length != 0)
+            {
+                // Make sure that the connection is still up and there is a message to send
+                if (socket != null)
+                {
+                    string message = MessageTextBox.Text;
+                    writer.WriteUInt32((uint)message.Length);
+                    writer.WriteString(message);
+
+                    ConversationListBox.Items.Add("Sent: " + message);
+                    // Clear the messageTextBox for a new message
+                    MessageTextBox.Text = "";
+
+                    await writer.StoreAsync();
+
+                }
+                else
+                {
+                    NotifyUser("No clients connected, please wait for a client to connect before attempting to send a message", NotifyType.StatusMessage);
+                }
+            }
+        }
+
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            Disconnect();
+            NotifyUser("Disconnected.", NotifyType.StatusMessage);
+        }
+
+        private async void Disconnect()
+        {
+            if (rfcommProvider != null)
+            {
+                rfcommProvider.StopAdvertising();
+                rfcommProvider = null;
+            }
+
+            if (socketListener != null)
+            {
+                socketListener.Dispose();
+                socketListener = null;
+            }
+
+            if (writer != null)
+            {
+                writer.DetachStream();
+                writer = null;
+            }
+
+            if (socket != null)
+            {
+                socket.Dispose();
+                socket = null;
+            }
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                ListenButton.IsEnabled = true;
+                DisconnectButton.IsEnabled = false;
+                ConversationListBox.Items.Clear();
+            });
+        }
+
+        /// <summary>
+        /// Invoked when the socket listener accepts an incoming Bluetooth connection.
+        /// </summary>
+        /// <param name="sender">The socket listener that accepted the connection.</param>
+        /// <param name="args">The connection accept parameters, which contain the connected socket.</param>
+        private async void OnConnectionReceived(
+            StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+            // Don't need the listener anymore
+            socketListener.Dispose();
+            socketListener = null;
+
+            try
+            {
+                socket = args.Socket;
+            }
+            catch (Exception e)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    NotifyUser(e.Message, NotifyType.ErrorMessage);
+                });
+                Disconnect();
+                return;
+            }
+
+            // Note - this is the supported way to get a Bluetooth device from a given socket
+            var remoteDevice = await BluetoothDevice.FromHostNameAsync(socket.Information.RemoteHostName);
+
+            writer = new DataWriter(socket.OutputStream);
+            var reader = new DataReader(socket.InputStream);
+            bool remoteDisconnection = false;
+
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                NotifyUser("Connected to Client: " + remoteDevice.Name, NotifyType.StatusMessage);
+            });
+
+            // Infinite read buffer loop
+            while (true)
+            {
+                try
+                {
+                    // Based on the protocol we've defined, the first uint is the size of the message
+                    uint readLength = await reader.LoadAsync(sizeof(uint));
+
+                    // Check if the size of the data is expected (otherwise the remote has already terminated the connection)
+                    if (readLength < sizeof(uint))
+                    {
+                        remoteDisconnection = true;
+                        break;
+                    }
+                    uint currentLength = reader.ReadUInt32();
+
+                    // Load the rest of the message since you already know the length of the data expected.  
+                    readLength = await reader.LoadAsync(currentLength);
+
+                    // Check if the size of the data is expected (otherwise the remote has already terminated the connection)
+                    if (readLength < currentLength)
+                    {
+                        remoteDisconnection = true;
+                        break;
+                    }
+                    string message = reader.ReadString(currentLength);
+
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        ConversationListBox.Items.Add("Received: " + message);
+                    });
+                }
+                // Catch exception HRESULT_FROM_WIN32(ERROR_OPERATION_ABORTED).
+                catch (Exception ex) when ((uint)ex.HResult == 0x800703E3)
+                {
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        NotifyUser("Client Disconnected Successfully", NotifyType.StatusMessage);
+                    });
+                    break;
+                }
+            }
+
+            reader.DetachStream();
+            if (remoteDisconnection)
+            {
+                Disconnect();
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    NotifyUser("Client disconnected", NotifyType.StatusMessage);
+                });
+            }
+        }
+    }
+}
---
+++ Win10Notifications/SampleConfiguration.cs
@@ -0,0 +1,43 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace Win10Notifications
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Bluetooth Rfcomm Chat Sample";
+    }
+
+    /// <summary>
+    /// Class containing Attributes and UUIDs that will populate the SDP record.
+    /// </summary>
+    class Constants
+    {
+        // The Chat Server's custom service Uuid: 34B1CF4D-1069-4AD6-89B6-E161D79BE4D8
+        public static readonly Guid RfcommChatServiceUuid = Guid.Parse("fa87c0d0-afac-11de-8a39-0800200c9a66");
+
+        // The Id of the Service Name SDP attribute
+        public const UInt16 SdpServiceNameAttributeId = 0x100;
+
+        // The SDP Type of the Service Name SDP attribute.
+        // The first byte in the SDP Attribute encodes the SDP Attribute Type as follows :
+        //    -  the Attribute Type size in the least significant 3 bits,
+        //    -  the SDP Attribute Type value in the most significant 5 bits.
+        public const byte SdpServiceNameAttributeType = (4 << 3) | 5;
+
+        // The value of the Service Name SDP attribute
+        public const string SdpServiceName = "Bluetooth Rfcomm Chat Service";
+    }
+}
